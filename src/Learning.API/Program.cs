0ee8039eb6a72228a463d55041b4c9f35020b336
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-11-30 23:56:31 +0200

    Add Migrations to Db

--- src/Learning.API/Program.cs
+++ src/Learning.API/Program.cs
@@ -1,6 +1,13 @@
+using Learning.Infrastructure.Data;
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContextPool<AppDbContext>(options 
+    => options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddControllers();
 
