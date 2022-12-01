7b7166e06413435fb4abd0c93e403169f4bcbdd6
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-12-01 03:34:15 +0200

    Added Serilog to App

---
+++ src/Learning.Logging/Serilogger.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Learning.Logging;
+
+public static class SeriLogger
+{
+    public static Action<HostBuilderContext, LoggerConfiguration> Configure => (context, configuration) =>
+       {
+           configuration
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+#if DEBUG
+                .WriteTo.Debug()
+                .WriteTo.Console()
+#endif
+                .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                .ReadFrom.Configuration(context.Configuration);
+       };
+}
