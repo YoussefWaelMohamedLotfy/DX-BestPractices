4fe0ed4ed6a6ce2280cd2491c54d8874e0da4ade
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-12-01 03:11:26 +0200

    Added Options for Database

--- src/Learning.API/Program.cs
+++ src/Learning.API/Program.cs
@@ -2,19 +2,32 @@ using Learning.Logging;
 using Serilog;
 
 using Learning.API.Extensions;
+using Learning.API.Options;
 using Learning.Infrastructure.Data;
 using Learning.Infrastructure.Extensions;
 using Learning.Infrastructure.Repositories;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Host.UseSerilog(SeriLogger.Configure);
 
-builder.Services.AddDbContextPool<AppDbContext>(options
-    => options.UseSqlite(builder.Configuration.GetConnectionString("Default")))
+builder.Services.ConfigureOptions<DatabaseOptionsSetup>();
+
+builder.Services.AddDbContextPool<AppDbContext>((serviceProvider, options) =>
+{
+    var dbOptions = serviceProvider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+
+    options.UseSqlite(dbOptions.ConnectionString, sqliteOptions =>
+    {
+        sqliteOptions.CommandTimeout(dbOptions.CommandTimeout);
+    });
+    options.EnableDetailedErrors(dbOptions.EnableDetailedErrors);
+    options.EnableSensitiveDataLogging(dbOptions.EnableSensitiveDataLogging);
+})
     .AddScoped<CourseRepository>();
 
 builder.Services.AddControllers();
@@ -25,7 +38,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 {
     app.MigrateDatabase<AppDbContext>(async (context, services) =>
     {
