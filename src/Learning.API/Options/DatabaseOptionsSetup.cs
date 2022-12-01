c309fe6da0a1248d1e59264ae1975f6287369c01
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-12-01 03:11:26 +0200

    Added Options for Database

---
+++ src/Learning.API/Options/DatabaseOptionsSetup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Learning.API.Options;
+
+internal sealed class DatabaseOptionsSetup : IConfigureOptions<DatabaseOptions>
+{
+    private const string ConfigurationSectionName = "DatabaseOptions";
+    private readonly IConfiguration _configuration;
+
+    public DatabaseOptionsSetup(IConfiguration configuration) 
+        => _configuration = configuration;
+
+    public void Configure(DatabaseOptions options)
+    {
+        options.ConnectionString = _configuration.GetConnectionString("Default");
+
+        _configuration.GetSection(ConfigurationSectionName).Bind(options);
+    }
+}
